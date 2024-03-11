58e45d116c6969ef33b817d8283194e179cab67b
Yossec <yosec.arratea22@gmail.com>
2024-03-11 00:15:35 -0500

    hook para formulario el useForm, recibe un objeto con los elementos del formulario y luedo del useForm lo desestructuramos sus valore

--- src/components/FormularioComponent.jsx
+++ src/components/FormularioComponent.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import { useForm } from '../hooks/useForm'
 
 export const FormularioComponent = () => {
+
+    const initialForm = {
+        userName: '',
+        email: '',
+        password: ''
+    }
+    const { formState, userName, email, password, onInputChange } = useForm(initialForm)
+
+    // const { userName, email, password} = formState
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(formState)
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="mb-3">
                 <label htmlFor="userName" className="form-label">Nombre de Usuario</label>
                 <input type="text"
                     className="form-control"
                     name="userName"
-                    placeholder="Nombre de usuario"/>
+                    placeholder="Nombre de usuario"
+                    value={userName}
+                    onChange={onInputChange}
+                />
 
             </div>
             <div className="mb-3">
@@ -16,7 +36,10 @@ export const FormularioComponent = () => {
                 <input type="email"
                     className="form-control" 
                     name="email" 
-                    placeholder='email'/>
+                    placeholder='email'
+                    value={email}
+                    onChange={onInputChange}
+                />
                 
             </div>
             <div className="mb-3">
@@ -24,7 +47,10 @@ export const FormularioComponent = () => {
                 <input type="password"   
                     className="form-control"
                     name="password"
-                    placeholder='password'/>
+                    placeholder='password'
+                    value={password}
+                    onChange={onInputChange}
+                />
             </div>
             <button type="submit" 
                 className="btn btn-primary">
