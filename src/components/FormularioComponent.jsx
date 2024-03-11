51d1ae5a7c2e555b47f98fc49e5f9e72938c2677
Yossec <yosec.arratea22@gmail.com>
2024-03-10 23:32:05 -0500

    modificando el diseño del boostrap a react

--- src/components/FormularioComponent.jsx
+++ src/components/FormularioComponent.jsx
@@ -4,19 +4,32 @@ export const FormularioComponent = () => {
     return (
         <form>
             <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label htmlFor="userName" className="form-label">Nombre de Usuario</label>
+                <input type="text"
+                    className="form-control"
+                    name="userName"
+                    placeholder="Nombre de usuario"/>
+
             </div>
             <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email"
+                    className="form-control" 
+                    name="email" 
+                    placeholder='email'/>
+                
             </div>
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password"   
+                    className="form-control"
+                    name="password"
+                    placeholder='password'/>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" 
+                className="btn btn-primary">
+                Submit
+            </button>
         </form>
     )
 }
